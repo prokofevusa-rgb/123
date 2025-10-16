b965c7bb48a95f29035327ef554fa9c9d62751a0
prokofevusa-rgb <prokofev.usa@gmail.com>
2025-10-16 15:30:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_db():
         port='5432',
         database='default_db',
         user='gen_user',
-        password= PASS_POSGRE_TIMEWEB,
+        password= 'PASS_POSGRE_TIMEWEB',
         sslmode='disable'
     )
 
